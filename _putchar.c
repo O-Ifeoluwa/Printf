9f667b3523a95f4ffdf94639bd8c28234e0fd37e
Ajileyeseun <99264815+Ajileyeseun@users.noreply.github.com>
2022-04-19 23:56:41 +0100

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
-* _putchar - writes the character to standard output
+* _putchar - writes the character to standard output.
 * @c: the character to print
 * Return: 0 on success, 1 on error and errno is set
 */
